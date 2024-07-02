209ca42e480974a2dbc82123f3004233a7d1c510
shariar-duke <shariarmahmud225@gmail.com>
2024-07-02 12:43:04 +0600

    changes in interceptive route

--- app/[lang]/(.)photos/[id]/page.jsx
+++ app/[lang]/(.)photos/[id]/page.jsx
@@ -1,13 +1,13 @@
-import PhotoDetails from "@/app/components/PhotoDetails";
 import Modal from "@/app/components/Modal";
+import PhotoDetails from "@/app/components/PhotoDetails";
 
-const PhotoModal =({params:{id, lang}})=> 
-{
-    return (
+const PhotoModal = ({params: {id, lang}}) => {
+
+    return(
         <Modal>
-            <PhotoDetails id={id} lang={lang}/>
+            <PhotoDetails id={id} lang={lang} />
         </Modal>
     )
 }
 
-export default PhotoModal
+export default PhotoModal;
